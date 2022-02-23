0d427e01bfdbf545313bff7dfe9509ff8b00fda2
Crowdbotics <engineering@crowdbotics.com>
2022-02-23 14:56:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testresend_gh_invite_1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testresend_gh_invite_1;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testresend_gh_invite_1";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testresend_gh_invite_1",
+  url: "https://testresend_gh_invite_1.botics.co",
+  api: "https://testresend_gh_invite_1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
